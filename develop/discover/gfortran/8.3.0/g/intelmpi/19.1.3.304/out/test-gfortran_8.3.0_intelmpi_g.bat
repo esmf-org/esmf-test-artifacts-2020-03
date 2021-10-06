705edc82561295a41ae1bc5c2d7d4592f43908db
Mark.Potts <Mark.Potts@noaa.gov>
2021-10-06 03:30:35 -0400

    update for test of gfortran_8.3.0_intelmpi_g_develop with hash ESMF_8_3_0_beta_snapshot_00-1-ge61c3b7 on discover [ci skip]

--- develop/discover/gfortran/8.3.0/g/intelmpi/19.1.3.304/out/test-gfortran_8.3.0_intelmpi_g.bat
+++ develop/discover/gfortran/8.3.0/g/intelmpi/19.1.3.304/out/test-gfortran_8.3.0_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Wed Oct 6 02:38:41 EDT 2021
+Wed Oct 6 03:29:51 EDT 2021
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-gfortran_8.3.0_intelmpi_g.bat_%j.o
--- release_8.2.0/discover/gfortran/10.1.0/g/intelmpi/19.1.3.304/out/build-gfortran_10.1.0_intelmpi_g.bat
+++ release_8.2.0/discover/gfortran/10.1.0/g/intelmpi/19.1.3.304/out/build-gfortran_10.1.0_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Wed Oct 6 02:38:46 EDT 2021
+Wed Oct 6 03:29:57 EDT 2021
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-gfortran_10.1.0_intelmpi_g.bat_%j.o
