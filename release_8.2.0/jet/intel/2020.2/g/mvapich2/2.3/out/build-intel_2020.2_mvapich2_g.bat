52b0f54861ec5c41aec8914325c4d0022729a04e
Mark A Potts <Mark.Potts@noaa.gov>
2021-10-06 04:10:58 +0000

    update for build of intel_2020.2_mvapich2_g_release_8.2.0 with hash ESMF_8_2_0_beta_snapshot_21 on jet [ci skip]

---
+++ release_8.2.0/jet/intel/2020.2/g/mvapich2/2.3/out/build-intel_2020.2_mvapich2_g.bat
@@ -0,0 +1,31 @@
+Wed Oct 6 04:10:56 GMT 2021
+#!/bin/sh -l
+#SBATCH --account=hfv3gfs
+#SBATCH -o build-intel_2020.2_mvapich2_g.bat_%j.o
+#SBATCH -e build-intel_2020.2_mvapich2_g.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=xjet
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+export ESMF_MPIRUN=mpirun.srun
+export LIBRARY_PATH=$LIBRARY_PATH:/apps/mvapich2/2.3-intel/lib
+export ESMF_CXXCOMPILEOPTS="-I/apps/mvapich2/2.3-intel/include"
+export ESMF_F90COMPILEOPTS="-I/apps/mvapich2/2.3-intel/include"
+module load intel/2020.2 mvapich2/2.3 netcdf/4.7.0
+module load hdf5/1.10.6 
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/mnt/lfs4/HFIP/hfv3gfs/Mark.Potts/intel_2020.2_mvapich2_g_release_8.2.0
+export ESMF_COMPILER=intel
+export ESMF_COMM=mvapich2
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 24 2>&1| tee build_$JOBID.log
+
