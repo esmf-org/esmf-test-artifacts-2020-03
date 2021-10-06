b0ad74e108e4f588a32ca4f7c1c6151f022ddc32
Ryan Long <ryan.long@noaa.gov>
2021-10-06 12:35:16 -0600

    update for build of intel_18.0.5_openmpi_g_develop with hash ESMF_8_3_0_beta_snapshot_00-1-ge61c3b7 on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_g.bat
+++ develop/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_g.bat
@@ -1,20 +1,20 @@
-Wed Oct 6 07:17:15 MDT 2021
+Wed Oct 6 12:35:15 MDT 2021
 #!/bin/sh -l
 #PBS -N test-intel_18.0.5_openmpi_g.bat
 #PBS -l walltime=3:00:00
 #PBS -q regular
-#PBS -A p48503002
+#PBS -A p93300606
 #PBS -l select=1:ncpus=36:mpiprocs=36
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /glade/scratch/mpotts/intel_18.0.5_openmpi_g_develop
+cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_openmpi_g_develop
 module load intel/18.0.5 openmpi/3.1.4 netcdf/4.6.3
 module list >& module-test.log
 
 set -x
 export ESMF_NETCDF=nc-config
 
-export ESMF_DIR=/glade/scratch/mpotts/intel_18.0.5_openmpi_g_develop
+export ESMF_DIR=/glade/scratch/rlong/esmf-testing/intel_18.0.5_openmpi_g_develop
 export ESMF_COMPILER=intel
 export ESMF_COMM=openmpi
 export ESMF_BOPT='g'
@@ -23,19 +23,19 @@ export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
-ssh cheyenne6 /glade/scratch/mpotts//glade/scratch/mpotts/intel_18.0.5_openmpi_g_develop/getres-int.sh
+ssh cheyenne6 /glade/scratch/rlong/esmf-testing//glade/scratch/rlong/esmf-testing/intel_18.0.5_openmpi_g_develop/getres-int.sh
 export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 chmod +x runpython.sh
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
-ssh cheyenne6 /glade/scratch/mpotts//glade/scratch/mpotts/intel_18.0.5_openmpi_g_develop/getres-int.sh
+ssh cheyenne6 /glade/scratch/rlong/esmf-testing//glade/scratch/rlong/esmf-testing/intel_18.0.5_openmpi_g_develop/getres-int.sh
 
 cd ../src/addon/ESMPy
 
 export PATH=$PATH:$HOME/.local/bin
 python3 setup.py build 2>&1 | tee python_build.log
-ssh cheyenne6 /glade/scratch/mpotts/intel_18.0.5_openmpi_g_develop/runpython.sh 2>&1 | tee python_build.log
+ssh cheyenne6 /glade/scratch/rlong/esmf-testing/intel_18.0.5_openmpi_g_develop/runpython.sh 2>&1 | tee python_build.log
 python3 setup.py test 2>&1 | tee python_test.log
 python3 setup.py test_examples 2>&1 | tee python_examples.log
 python3 setup.py test_regrid_from_file 2>&1 | tee python_regrid.log
