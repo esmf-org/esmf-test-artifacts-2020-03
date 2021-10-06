6e0b82d4c7d91ac9f41ee205cdf426b19103fa4d
Mark A Potts <Mark.Potts@noaa.gov>
2021-10-06 08:26:01 +0000

    update for test of pgi_18.1_intelmpi_O_develop with hash ESMF_8_3_0_beta_snapshot_00-1-ge61c3b7 on hera [ci skip]

--- develop/hera/pgi/18.1/O/intelmpi/2018.0.4/out/test-pgi_18.1_intelmpi_O.bat
+++ develop/hera/pgi/18.1/O/intelmpi/2018.0.4/out/test-pgi_18.1_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Oct 6 07:38:44 UTC 2021
+Wed Oct 6 08:25:58 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-pgi_18.1_intelmpi_O.bat_%j.o
--- develop/hera/pgi/18.1/g/intelmpi/2018.0.4/out/test-pgi_18.1_intelmpi_g.bat
+++ develop/hera/pgi/18.1/g/intelmpi/2018.0.4/out/test-pgi_18.1_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Wed Oct 6 08:15:23 UTC 2021
+Wed Oct 6 08:15:49 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-pgi_18.1_intelmpi_g.bat_%j.o
