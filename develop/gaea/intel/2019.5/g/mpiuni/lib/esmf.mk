aa316de14cb344265eef6befda44f2612a809112
Mark Potts <Mark.Potts@noaa.gov>
2021-10-06 02:15:21 -0400

    update for test of intel_2019.5_mpi_g_release_8.2.0 with hash ESMF_8_2_0_beta_snapshot_21 on gaea [ci skip]

--- develop/gaea/intel/2019.5/g/mpiuni/lib/esmf.mk
+++ develop/gaea/intel/2019.5/g/mpiuni/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-10-05 00:45:59
+build time -- 2021-10-06 00:52:38
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
@@ -13,10 +13,10 @@ build time -- 2021-10-05 00:45:59
 #
 
 #----------------------------------------------
-ESMF_VERSION_STRING=8.2.0 beta snapshot
+ESMF_VERSION_STRING=8.3.0 beta snapshot
 
 ESMF_VERSION_MAJOR=8
-ESMF_VERSION_MINOR=2
+ESMF_VERSION_MINOR=3
 ESMF_VERSION_REVISION=0
 ESMF_VERSION_PATCHLEVEL=0
 ESMF_VERSION_PUBLIC='F'
--- develop/gaea/intel/2019.5/g/mpiuni/out/build-intel_2019.5_mpiuni_g.bat
+++ develop/gaea/intel/2019.5/g/mpiuni/out/build-intel_2019.5_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Wed Oct 6 00:52:54 EDT 2021
+Wed Oct 6 02:09:56 EDT 2021
 #!/bin/sh -l
 #SBATCH --account=nggps_emc
 #SBATCH -o build-intel_2019.5_mpiuni_g.bat_%j.o
