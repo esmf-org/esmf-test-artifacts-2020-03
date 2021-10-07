b75afec5ebc1f4a0ee653f7a2889f1e6547ef9a8
Mark A Potts <Mark.Potts@noaa.gov>
2021-10-07 06:02:34 +0000

    update for build of intel_18.0.4_intelmpi_O_release_8.2.0 with hash ESMF_8_2_0_beta_snapshot_21 on hera [ci skip]

--- release_8.2.0/hera/intel/18.0.4/O/intelmpi/2018.4.274/out/build-intel_18.0.4_intelmpi_O.bat
+++ release_8.2.0/hera/intel/18.0.4/O/intelmpi/2018.4.274/out/build-intel_18.0.4_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Oct 6 07:33:23 UTC 2021
+Thu Oct 7 06:02:31 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-intel_18.0.4_intelmpi_O.bat_%j.o
--- release_8.2.0/hera/intel/18.0.4/O/intelmpi/2018.4.274/out/test-intel_18.0.4_intelmpi_O.bat
+++ release_8.2.0/hera/intel/18.0.4/O/intelmpi/2018.4.274/out/test-intel_18.0.4_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Oct 6 07:33:24 UTC 2021
+Thu Oct 7 06:02:31 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-intel_18.0.4_intelmpi_O.bat_%j.o
