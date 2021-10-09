2d4d43b677b499843f67371b42b0562697063c2b
Mark.Potts <Mark.Potts@noaa.gov>
2021-10-09 01:58:04 -0400

    update for build of pgi_2020_mpiuni_O_release_8.2.0 with hash ESMF_8_2_0_beta_snapshot_21-1-g86aff4e on discover [ci skip]

--- release_8.2.0/discover/pgi/2020/O/mpiuni/out/build-pgi_2020_mpiuni_O.bat
+++ release_8.2.0/discover/pgi/2020/O/mpiuni/out/build-pgi_2020_mpiuni_O.bat
@@ -1,15 +1,16 @@
-Fri Oct 8 02:25:41 EDT 2021
+Sat Oct 9 01:57:53 EDT 2021
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-pgi_2020_mpiuni_O.bat_%j.o
 #SBATCH -e build-pgi_2020_mpiuni_O.bat_%j.e
-#SBATCH --time=1:20:00
+#SBATCH --time=2:00:00
 #SBATCH --partition=compute
 #SBATCH --qos=allnccs
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=28
 #SBATCH --exclusive
 export JOBID=$SLURM_JOBID
+export ESMF_MPIRUN=/gpfsm/dnb04/projects/p98/mpotts/esmf/pgi_2020_mpiuni_O_release_8.2.0/src/Infrastructure/stubs/mpiuni/mpirun
 module load comp/pgi/20.4  
 
 module list >& module-build.log
