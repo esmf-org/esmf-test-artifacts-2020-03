351ee7e90c98408bd52f3a04a724ae5d82770e52
Mark Potts <Mark.Potts@noaa.gov>
2021-10-10 04:15:06 -0400

    update for build of gfortran_9.3.0_openmpi_g_develop with hash ESMF_8_3_0_beta_snapshot_00-2-g6354f29 on chianti [ci skip]

--- develop/chianti/gfortran/9.3.0/g/openmpi/4.0.5-gcc-9.3.0/out/test-gfortran_9.3.0_openmpi_g.bat
+++ develop/chianti/gfortran/9.3.0/g/openmpi/4.0.5-gcc-9.3.0/out/test-gfortran_9.3.0_openmpi_g.bat
@@ -1,4 +1,4 @@
-Sat Oct 9 04:28:18 EDT 2021
+Sun Oct 10 04:09:46 EDT 2021
 #!/bin/bash -l
 export JOBID=12346
 
--- develop/chianti/intel/2021.2/g/intelmpi/2021.2.0-gcc-9.3.0/lib/esmf.mk
+++ develop/chianti/intel/2021.2/g/intelmpi/2021.2.0-gcc-9.3.0/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-10-09 03:07:21.361177
+build time -- 2021-10-10 03:08:58.118624
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
