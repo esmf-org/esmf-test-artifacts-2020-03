1d3c4ec5064888955b7bc77b9601d2fc8c88b02a
Mark Potts <mark.potts@noaa.gov>
2021-10-12 05:32:06 -0600

    update for test of intel_18.0.5_mpt_g_release_8.2.0 with hash ESMF_8_2_0_beta_snapshot_21-3-ge04105f on cheyenne [ci skip]

--- release_8.2.0/cheyenne/intel/18.0.5/g/mpt/2.19/out/build-intel_18.0.5_mpt_g.bat
+++ release_8.2.0/cheyenne/intel/18.0.5/g/mpt/2.19/out/build-intel_18.0.5_mpt_g.bat
@@ -1,4 +1,4 @@
-Tue Oct 12 04:13:22 MDT 2021
+Tue Oct 12 05:31:16 MDT 2021
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_mpt_g.bat
 #PBS -l walltime=1:00:00
--- release_8.2.0/cheyenne/intel/18.0.5/g/mpt/2.19/out/test-intel_18.0.5_mpt_g.bat
+++ release_8.2.0/cheyenne/intel/18.0.5/g/mpt/2.19/out/test-intel_18.0.5_mpt_g.bat
@@ -1,4 +1,4 @@
-Tue Oct 12 04:13:22 MDT 2021
+Tue Oct 12 05:31:16 MDT 2021
 #!/bin/sh -l
 #PBS -N test-intel_18.0.5_mpt_g.bat
 #PBS -l walltime=3:00:00
