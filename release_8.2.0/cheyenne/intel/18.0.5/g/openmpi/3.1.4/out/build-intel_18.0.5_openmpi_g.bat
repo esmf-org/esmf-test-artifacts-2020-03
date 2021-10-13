5d82229344ef00f164c0dca56a865c087de33a9b
Mark Potts <mark.potts@noaa.gov>
2021-10-13 13:30:32 -0600

    update for build of intel_18.0.5_openmpi_g_release_8.2.0 with hash ESMF_8_2_0_beta_snapshot_22-1-g094508a on cheyenne [ci skip]

--- release_8.2.0/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_g.bat
+++ release_8.2.0/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_g.bat
@@ -1,4 +1,4 @@
-Wed Oct 13 07:11:00 MDT 2021
+Wed Oct 13 13:30:30 MDT 2021
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_openmpi_g.bat
 #PBS -l walltime=1:00:00
@@ -9,6 +9,8 @@ Wed Oct 13 07:11:00 MDT 2021
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
 cd /glade/scratch/mpotts/intel_18.0.5_openmpi_g_release_8.2.0
+
+module load python
 module load intel/18.0.5 openmpi/3.1.4 netcdf/4.6.3
 module list >& module-build.log
 
