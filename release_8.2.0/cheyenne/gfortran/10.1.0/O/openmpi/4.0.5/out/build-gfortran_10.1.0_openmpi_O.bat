9995da8b2c85c260b07f3ffe96a00cfa742938c9
Mark Potts <mark.potts@noaa.gov>
2021-10-13 03:50:08 -0600

    update for build of gfortran_10.1.0_openmpi_O_release_8.2.0 with hash ESMF_8_2_0_beta_snapshot_22-1-g094508a on cheyenne [ci skip]

--- release_8.2.0/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/build-gfortran_10.1.0_openmpi_O.bat
+++ release_8.2.0/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/build-gfortran_10.1.0_openmpi_O.bat
@@ -1,4 +1,4 @@
-Tue Oct 12 04:31:38 MDT 2021
+Wed Oct 13 03:50:06 MDT 2021
 #!/bin/sh -l
 #PBS -N build-gfortran_10.1.0_openmpi_O.bat
 #PBS -l walltime=1:00:00
--- release_8.2.0/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/test-gfortran_10.1.0_openmpi_O.bat
+++ release_8.2.0/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/test-gfortran_10.1.0_openmpi_O.bat
@@ -1,4 +1,4 @@
-Tue Oct 12 04:31:38 MDT 2021
+Wed Oct 13 03:50:06 MDT 2021
 #!/bin/sh -l
 #PBS -N test-gfortran_10.1.0_openmpi_O.bat
 #PBS -l walltime=2:00:00
