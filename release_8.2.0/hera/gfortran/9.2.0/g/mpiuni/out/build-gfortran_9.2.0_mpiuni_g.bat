aec84929e05a67d26a0cd3c97ceea3b92edd2ee0
Mark A Potts <Mark.Potts@noaa.gov>
2021-10-14 07:06:22 +0000

    update for build of gfortran_9.2.0_mpiuni_g_release_8.2.0 with hash ESMF_8_2_0_beta_snapshot_22-2-g9510bd0 on hera [ci skip]

--- release_8.2.0/hera/gfortran/9.2.0/g/mpiuni/out/build-gfortran_9.2.0_mpiuni_g.bat
+++ release_8.2.0/hera/gfortran/9.2.0/g/mpiuni/out/build-gfortran_9.2.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Wed Oct 13 09:04:17 UTC 2021
+Thu Oct 14 07:06:20 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-gfortran_9.2.0_mpiuni_g.bat_%j.o
@@ -22,6 +22,7 @@ export ESMF_NETCDF_INCLUDE=$NETCDF/include
 export ESMF_NETCDF_LIBPATH=$NETCDF/lib
 export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf -lhdf5_hl -lhdf5 $HDF5ExtraLibs"
 export ESMF_NETCDF=nc-config
+tar xvfz pytest-input.tar.gz
 export ESMF_DIR=/scratch1/NCEPDEV/stmp2/Mark.Potts/gfortran_9.2.0_mpiuni_g_release_8.2.0
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=mpiuni
