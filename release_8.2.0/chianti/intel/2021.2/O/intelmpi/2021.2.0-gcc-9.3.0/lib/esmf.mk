5d4d18b77a18a9d0895ea9ab71b29e2e75a0791e
Mark Potts <Mark.Potts@noaa.gov>
2021-10-15 02:00:09 -0400

    update for build of gfortran_9.3.0_openmpi_O_release_8.2.0 with hash ESMF_8_2_0_beta_snapshot_22-10-g6974ca4 on chianti [ci skip]

--- release_8.2.0/chianti/intel/2021.2/O/intelmpi/2021.2.0-gcc-9.3.0/lib/esmf.mk
+++ release_8.2.0/chianti/intel/2021.2/O/intelmpi/2021.2.0-gcc-9.3.0/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-10-14 01:26:59.653855
+build time -- 2021-10-15 01:25:26.475444
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
@@ -14,7 +14,7 @@ build time -- 2021-10-14 01:26:59.653855
 
 #----------------------------------------------
 ESMF_VERSION_STRING=8.2.0 beta snapshot
-ESMF_VERSION_STRING_GIT=ESMF_8_2_0_beta_snapshot_22-2-g9510bd0f2c
+ESMF_VERSION_STRING_GIT=ESMF_8_2_0_beta_snapshot_22-10-g6974ca47a6
 #----------------------------------------------
 
 ESMF_VERSION_MAJOR=8
@@ -34,7 +34,7 @@ ESMF_F90LINKER=mpiifort
 
 ESMF_F90COMPILEOPTS=-O -fPIC -assume realloc_lhs -m64 -mcmodel=small -pthread -threads  -qopenmp
 ESMF_F90COMPILEPATHS=-I/home/mpotts/intel_2021.2_intelmpi_O_release_8.2.0/mod/modO/Linux.intel.64.intelmpi.default -I/home/mpotts/intel_2021.2_intelmpi_O_release_8.2.0/src/include -I/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/intel-2021.2.0/netcdf-c-4.8.0-vokw5hlljhlh222zbdgaxw32qvvknpqz/include -I/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/intel-2021.2.0/netcdf-fortran-4.5.3-x5ziod6tk2uyvts5hwrauoc2h7hxphbo/include
-ESMF_F90COMPILECPPFLAGS=-DESMF_NO_INTEGER_1_BYTE -DESMF_NO_INTEGER_2_BYTE -DESMF_VERSION_STRING_GIT='ESMF_8_2_0_beta_snapshot_22-2-g9510bd0f2c' -DESMF_MOAB=1 -DESMF_LAPACK=1 -DESMF_LAPACK_INTERNAL=1 -DESMF_NO_ACC_SOFTWARE_STACK=1 -DESMF_NETCDF=1 -DESMF_YAMLCPP=1 -DESMF_YAML=1 -DESMF_PIO=1 -DESMF_MPIIO -DESMF_NO_OPENACC -DESMF_TESTEXHAUSTIVE -DESMF_BOPT_O -DESMF_TESTCOMPTUNNEL -DESMF_TESTWITHTHREADS -DSx86_64_small=1 -DESMF_OS_Linux=1 -DESMF_COMM=intelmpi -DESMF_DIR=/home/mpotts/intel_2021.2_intelmpi_O_release_8.2.0
+ESMF_F90COMPILECPPFLAGS=-DESMF_NO_INTEGER_1_BYTE -DESMF_NO_INTEGER_2_BYTE -DESMF_VERSION_STRING_GIT='ESMF_8_2_0_beta_snapshot_22-10-g6974ca47a6' -DESMF_MOAB=1 -DESMF_LAPACK=1 -DESMF_LAPACK_INTERNAL=1 -DESMF_NO_ACC_SOFTWARE_STACK=1 -DESMF_NETCDF=1 -DESMF_YAMLCPP=1 -DESMF_YAML=1 -DESMF_PIO=1 -DESMF_MPIIO -DESMF_NO_OPENACC -DESMF_TESTEXHAUSTIVE -DESMF_BOPT_O -DESMF_TESTCOMPTUNNEL -DESMF_TESTWITHTHREADS -DSx86_64_small=1 -DESMF_OS_Linux=1 -DESMF_COMM=intelmpi -DESMF_DIR=/home/mpotts/intel_2021.2_intelmpi_O_release_8.2.0
 ESMF_F90COMPILEFREECPP=
 ESMF_F90COMPILEFREENOCPP=
 ESMF_F90COMPILEFIXCPP=
@@ -53,7 +53,7 @@ ESMF_CXXLINKER=mpiicpc
 
 ESMF_CXXCOMPILEOPTS=-std=c++11 -O -DNDEBUG -fPIC -m64 -mcmodel=small -pthread  -qopenmp
 ESMF_CXXCOMPILEPATHS= -I/home/mpotts/intel_2021.2_intelmpi_O_release_8.2.0/src/include  -I/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/intel-2021.2.0/netcdf-c-4.8.0-vokw5hlljhlh222zbdgaxw32qvvknpqz/include -I/home/mpotts/intel_2021.2_intelmpi_O_release_8.2.0/src/prologue/yaml-cpp/include
-ESMF_CXXCOMPILECPPFLAGS=-DESMF_NO_INTEGER_1_BYTE -DESMF_NO_INTEGER_2_BYTE -DESMF_VERSION_STRING_GIT='ESMF_8_2_0_beta_snapshot_22-2-g9510bd0f2c' -DESMF_MOAB=1 -DESMF_LAPACK=1 -DESMF_LAPACK_INTERNAL=1 -DESMF_NO_ACC_SOFTWARE_STACK=1 -DESMF_NETCDF=1 -DESMF_YAMLCPP=1 -DESMF_YAML=1 -DESMF_PIO=1 -DESMF_MPIIO -DESMF_NO_OPENACC -DESMF_TESTEXHAUSTIVE -DESMF_BOPT_O -DESMF_TESTCOMPTUNNEL -DESMF_TESTWITHTHREADS -DSx86_64_small=1 -DESMF_OS_Linux=1 -DESMF_COMM=intelmpi -DESMF_DIR=/home/mpotts/intel_2021.2_intelmpi_O_release_8.2.0 -D__SDIR__='' -DESMF_CXXSTD=11
+ESMF_CXXCOMPILECPPFLAGS=-DESMF_NO_INTEGER_1_BYTE -DESMF_NO_INTEGER_2_BYTE -DESMF_VERSION_STRING_GIT='ESMF_8_2_0_beta_snapshot_22-10-g6974ca47a6' -DESMF_MOAB=1 -DESMF_LAPACK=1 -DESMF_LAPACK_INTERNAL=1 -DESMF_NO_ACC_SOFTWARE_STACK=1 -DESMF_NETCDF=1 -DESMF_YAMLCPP=1 -DESMF_YAML=1 -DESMF_PIO=1 -DESMF_MPIIO -DESMF_NO_OPENACC -DESMF_TESTEXHAUSTIVE -DESMF_BOPT_O -DESMF_TESTCOMPTUNNEL -DESMF_TESTWITHTHREADS -DSx86_64_small=1 -DESMF_OS_Linux=1 -DESMF_COMM=intelmpi -DESMF_DIR=/home/mpotts/intel_2021.2_intelmpi_O_release_8.2.0 -D__SDIR__='' -DESMF_CXXSTD=11
 
 ESMF_CXXLINKOPTS=  -m64 -mcmodel=small -pthread -Wl,--no-as-needed  -qopenmp
 ESMF_CXXLINKPATHS=-L/home/mpotts/intel_2021.2_intelmpi_O_release_8.2.0/lib/libO/Linux.intel.64.intelmpi.default -L/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/intel-2021.2.0/netcdf-c-4.8.0-vokw5hlljhlh222zbdgaxw32qvvknpqz/lib -L/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/intel-2021.2.0/netcdf-fortran-4.5.3-x5ziod6tk2uyvts5hwrauoc2h7hxphbo/lib -L/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-9.3.0/intel-oneapi-compilers-2021.2.0-jfovfoypl6hwupdt6z6cedtsv3ebigq3/compiler/2021.2.0/linux/bin/intel64/../../compiler/lib/intel64_lin/
