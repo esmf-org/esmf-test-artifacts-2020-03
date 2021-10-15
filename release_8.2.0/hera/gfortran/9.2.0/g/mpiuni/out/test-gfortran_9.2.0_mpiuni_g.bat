174f72470f815911482487127aed206a53fcaadf
Mark Potts <Mark.Potts@noaa.gov>
2021-10-15 06:00:19 -0400

    update for test of intel_18.0.4_mpiuni_O_release_8.2.0 with hash ESMF_8_2_0_beta_snapshot_22-10-g6974ca4 on hera [ci skip]

--- release_8.2.0/hera/gfortran/9.2.0/g/mpiuni/out/test-gfortran_9.2.0_mpiuni_g.bat
+++ release_8.2.0/hera/gfortran/9.2.0/g/mpiuni/out/test-gfortran_9.2.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Fri Oct 15 09:11:21 UTC 2021
+Fri Oct 15 09:44:00 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-gfortran_9.2.0_mpiuni_g.bat_%j.o
--- release_8.2.0/hera/intel/18.0.4/O/mpiuni/out/test-intel_18.0.4_mpiuni_O.bat
+++ release_8.2.0/hera/intel/18.0.4/O/mpiuni/out/test-intel_18.0.4_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Fri Oct 15 09:18:42 UTC 2021
+Fri Oct 15 09:58:22 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-intel_18.0.4_mpiuni_O.bat_%j.o
