b466ff0a7a2ed9804f08689ab4f552fc6ec02d95
Mark Potts <Mark.Potts@noaa.gov>
2021-10-17 16:40:09 -0400

    update for test of gfortran_10.3.0_mpich3_g_release_8.2.0 with hash ESMF_8_2_0_beta_snapshot_22-10-g6974ca4 on acorn [ci skip]

--- release_8.2.0/acorn/intel/2019.3/O/mpi/8.1.7/lib/esmf.mk
+++ release_8.2.0/acorn/intel/2019.3/O/mpi/8.1.7/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-10-16 20:13:45
+build time -- 2021-10-17 20:14:13
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- release_8.2.0/acorn/intel/2019.3/g/mpi/8.1.7/lib/esmf.mk
+++ release_8.2.0/acorn/intel/2019.3/g/mpi/8.1.7/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-10-16 20:15:02
+build time -- 2021-10-17 20:14:58
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
