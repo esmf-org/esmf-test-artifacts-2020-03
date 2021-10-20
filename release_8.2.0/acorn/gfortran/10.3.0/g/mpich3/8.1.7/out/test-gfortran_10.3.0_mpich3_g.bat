b171aba2bb656bc837c390261175a252946f44e5
Mark Potts <Mark.Potts@noaa.gov>
2021-10-20 16:40:32 -0400

    update for test of gfortran_10.3.0_mpich3_g_release_8.2.0 with hash ESMF_8_2_0_beta_snapshot_22-14-g268d795 on acorn [ci skip]

--- release_8.2.0/acorn/gfortran/10.3.0/g/mpich3/8.1.7/out/test-gfortran_10.3.0_mpich3_g.bat
+++ release_8.2.0/acorn/gfortran/10.3.0/g/mpich3/8.1.7/out/test-gfortran_10.3.0_mpich3_g.bat
@@ -1,4 +1,4 @@
-Wed Oct 20 20:10:02 UTC 2021
+Wed Oct 20 20:37:05 UTC 2021
 #!/bin/sh -l
 #PBS -N test-gfortran_10.3.0_mpich3_g.bat
 #PBS -l walltime=1:00:00
--- release_8.2.0/acorn/intel/2019.3/O/mpi/8.1.7/out/build-intel_2019.3_mpi_O.bat
+++ release_8.2.0/acorn/intel/2019.3/O/mpi/8.1.7/out/build-intel_2019.3_mpi_O.bat
@@ -1,4 +1,4 @@
-Wed Oct 20 20:13:45 UTC 2021
+Wed Oct 20 20:36:48 UTC 2021
 #!/bin/sh -l
 #PBS -N build-intel_2019.3_mpi_O.bat
 #PBS -l walltime=1:00:00
