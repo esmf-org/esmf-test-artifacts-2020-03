4652c74742282374e34bc23dee82731c0e3f334e
Mark Potts <mark.potts@noaa.gov>
2021-10-30 06:27:50 +0000

    update for build of intel_2019.0.5_intelmpi_O_develop with hash ESMF_8_3_0_beta_snapshot_02 on s4 [ci skip]

--- develop/s4/intel/2019.0.5/O/intelmpi/19.0.5/out/build-intel_2019.0.5_intelmpi_O.bat
+++ develop/s4/intel/2019.0.5/O/intelmpi/19.0.5/out/build-intel_2019.0.5_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Fri Oct 8 07:16:00 UTC 2021
+Sat Oct 30 06:27:47 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=star
 #SBATCH -o build-intel_2019.0.5_intelmpi_O.bat_%j.o
--- develop/s4/intel/2019.0.5/O/intelmpi/19.0.5/out/test-intel_2019.0.5_intelmpi_O.bat
+++ develop/s4/intel/2019.0.5/O/intelmpi/19.0.5/out/test-intel_2019.0.5_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Fri Oct 8 07:16:00 UTC 2021
+Sat Oct 30 06:27:47 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=star
 #SBATCH -o test-intel_2019.0.5_intelmpi_O.bat_%j.o
@@ -29,10 +29,8 @@ export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
-ssh s4-submit.ssec.wisc.edu /scratch/users/mpotts//scratch/users/mpotts/intel_2019.0.5_intelmpi_O_develop/getres-int.sh
 export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 chmod +x runpython.sh
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
-ssh s4-submit.ssec.wisc.edu /scratch/users/mpotts//scratch/users/mpotts/intel_2019.0.5_intelmpi_O_develop/getres-int.sh
