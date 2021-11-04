f688d740b03c5f53c9056c976890aeb25877480b
Mark A Potts <Mark.Potts@noaa.gov>
2021-11-04 01:33:29 +0000

    update for build of intel_2019.3_mpi_g_develop with hash ESMF_8_3_0_beta_snapshot_02-1-gd594fca on acorn [ci skip]

--- develop/acorn/intel/2019.3/g/mpi/8.1.7/out/test-intel_2019.3_mpi_g.bat
+++ develop/acorn/intel/2019.3/g/mpi/8.1.7/out/test-intel_2019.3_mpi_g.bat
@@ -1,4 +1,4 @@
-Fri Oct 8 20:37:30 UTC 2021
+Thu Nov 4 01:33:28 UTC 2021
 #!/bin/sh -l
 #PBS -N test-intel_2019.3_mpi_g.bat
 #PBS -l walltime=1:00:00
@@ -39,13 +39,11 @@ export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
-ssh alogin01 /lfs/h1/emc/ptmp/Mark.Potts//lfs/h1/emc/ptmp/Mark.Potts/intel_2019.3_mpi_g_develop/getres-int.sh
 export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 chmod +x runpython.sh
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
-ssh alogin01 /lfs/h1/emc/ptmp/Mark.Potts//lfs/h1/emc/ptmp/Mark.Potts/intel_2019.3_mpi_g_develop/getres-int.sh
 
 cd ../src/addon/ESMPy
 
