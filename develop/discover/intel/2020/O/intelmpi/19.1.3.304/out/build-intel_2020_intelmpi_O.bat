b5a4bf20eef4ab245289b7120e46960781365ce0
Mark.Potts <Mark.Potts@noaa.gov>
2021-11-05 01:38:06 -0400

    update for build of intel_2020_intelmpi_O_develop with hash ESMF_8_3_0_beta_snapshot_02-1-gd594fca on discover [ci skip]

--- develop/discover/intel/2020/O/intelmpi/19.1.3.304/out/build-intel_2020_intelmpi_O.bat
+++ develop/discover/intel/2020/O/intelmpi/19.1.3.304/out/build-intel_2020_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Thu Nov 4 02:02:57 EDT 2021
+Fri Nov 5 01:37:53 EDT 2021
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-intel_2020_intelmpi_O.bat_%j.o
--- develop/discover/intel/2020/O/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_O.bat
+++ develop/discover/intel/2020/O/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Thu Nov 4 02:02:57 EDT 2021
+Fri Nov 5 01:37:53 EDT 2021
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-intel_2020_intelmpi_O.bat_%j.o
