e25c9a9119f6ff246e2787dcae6a61890abf59f2
Mark Potts <mpotts@redlineperf.com>
2021-11-08 02:56:35 -0600

    update for test of gfortran_8.3.0_mpiuni_g_develop with hash ESMF_8_3_0_beta_snapshot_02-1-gd594fca on orion [ci skip]

--- develop/orion/gfortran/8.3.0/g/mpiuni/out/build-gfortran_8.3.0_mpiuni_g.bat
+++ develop/orion/gfortran/8.3.0/g/mpiuni/out/build-gfortran_8.3.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Mon Nov 8 02:30:52 CST 2021
+Mon Nov 8 02:56:00 CST 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-gfortran_8.3.0_mpiuni_g.bat_%j.o
--- develop/orion/gfortran/8.3.0/g/mpiuni/out/test-gfortran_8.3.0_mpiuni_g.bat
+++ develop/orion/gfortran/8.3.0/g/mpiuni/out/test-gfortran_8.3.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Mon Nov 8 02:30:52 CST 2021
+Mon Nov 8 02:55:59 CST 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-gfortran_8.3.0_mpiuni_g.bat_%j.o
