8dfdda1d94115f652cf7ad98925824991fcd7f46
Mark Potts <Mark.Potts@noaa.gov>
2021-11-15 09:15:07 -0500

    update for test of intel_2020_intelmpi_O_develop with hash ESMF_8_3_0_beta_snapshot_02-9-g35b0f61 on orion [ci skip]

--- develop/orion/intel/2020/O/intelmpi/2020.2/lib/esmf.mk
+++ develop/orion/intel/2020/O/intelmpi/2020.2/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-11-14 02:35:42
+build time -- 2021-11-15 03:39:32
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/orion/intel/2020/O/intelmpi/2020.2/out/test-intel_2020_intelmpi_O.bat
+++ develop/orion/intel/2020/O/intelmpi/2020.2/out/test-intel_2020_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Mon Nov 15 03:40:02 CST 2021
+Mon Nov 15 08:04:47 CST 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-intel_2020_intelmpi_O.bat_%j.o
