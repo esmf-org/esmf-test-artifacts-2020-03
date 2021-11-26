134b78928406a930dd25fc1a5498ce2fed86b42f
Mark Potts <Mark.Potts@noaa.gov>
2021-11-26 02:00:06 -0500

    update for test of intel_18.0.4_mpiuni_g_develop with hash ESMF_8_3_0_beta_snapshot_03 on hera [ci skip]

--- develop/hera/gfortran/9.2.0/g/openmpi/3.1.4/lib/esmf.mk
+++ develop/hera/gfortran/9.2.0/g/openmpi/3.1.4/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-11-25 06:17:36
+build time -- 2021-11-26 06:15:00
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/hera/intel/18.0.4/g/mpiuni/out/test-intel_18.0.4_mpiuni_g.bat
+++ develop/hera/intel/18.0.4/g/mpiuni/out/test-intel_18.0.4_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Fri Nov 26 06:13:53 UTC 2021
+Fri Nov 26 06:52:03 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o test-intel_18.0.4_mpiuni_g.bat_%j.o
