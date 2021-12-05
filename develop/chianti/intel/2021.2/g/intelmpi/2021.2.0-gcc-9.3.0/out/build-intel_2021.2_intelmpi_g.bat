93b2ce377ffce3d4a4a812973d479437e4a63266
Role.Esmfmaint <esmfmaint@gmail.com>
2021-12-05 06:30:15 +0000

    update for test of gfortran_9.3.0_openmpi_g_develop with hash ESMF_8_3_0_beta_snapshot_04-7-gc7b07b1 on chianti [ci skip]

--- develop/chianti/intel/2021.2/g/intelmpi/2021.2.0-gcc-9.3.0/out/build-intel_2021.2_intelmpi_g.bat
+++ develop/chianti/intel/2021.2/g/intelmpi/2021.2.0-gcc-9.3.0/out/build-intel_2021.2_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Sat Dec 4 01:21:53 EST 2021
+Sun Dec 5 01:21:53 EST 2021
 #!/bin/bash -l
 export JOBID=12345
 module load intel-oneapi-compilers/2021.2.0-gcc-9.3.0 intel-oneapi-mpi/2021.2.0-gcc-9.3.0 netcdf-c/4.8.0-intel-2021.2.0
