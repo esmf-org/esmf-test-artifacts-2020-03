72d17687e3e13971ff52286eaa89819d8f89f59a
Mark Potts <Mark.Potts@noaa.gov>
2021-12-07 01:20:43 -0500

    update for build of intel_2021.2_intelmpi_g_develop with hash ESMF_8_3_0_beta_snapshot_04-7-gc7b07b1 on chianti [ci skip]

--- develop/chianti/intel/2021.2/g/intelmpi/2021.2.0-gcc-9.3.0/out/test-intel_2021.2_intelmpi_g.bat
+++ develop/chianti/intel/2021.2/g/intelmpi/2021.2.0-gcc-9.3.0/out/test-intel_2021.2_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Mon Dec 6 01:21:51 EST 2021
+Tue Dec 7 01:20:43 EST 2021
 #!/bin/bash -l
 export JOBID=12346
 module load intel-oneapi-compilers/2021.2.0-gcc-9.3.0 intel-oneapi-mpi/2021.2.0-gcc-9.3.0 netcdf-c/4.8.0-intel-2021.2.0
