063bdeccb88917b3e6bc9a6433c60648dbc4de0d
Role.Esmfmaint <esmfmaint@gmail.com>
2021-12-08 06:15:26 +0000

    update for build of intel_2021.2_intelmpi_O_develop with hash ESMF_8_3_0_beta_snapshot_04-7-gc7b07b1 on chianti [ci skip]

--- develop/chianti/intel/2021.2/O/intelmpi/2021.2.0-gcc-9.3.0/out/build-intel_2021.2_intelmpi_O.bat
+++ develop/chianti/intel/2021.2/O/intelmpi/2021.2.0-gcc-9.3.0/out/build-intel_2021.2_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Tue Dec 7 01:16:10 EST 2021
+Wed Dec 8 01:14:49 EST 2021
 #!/bin/bash -l
 export JOBID=12345
 module load intel-oneapi-compilers/2021.2.0-gcc-9.3.0 intel-oneapi-mpi/2021.2.0-gcc-9.3.0 netcdf-c/4.8.0-intel-2021.2.0
