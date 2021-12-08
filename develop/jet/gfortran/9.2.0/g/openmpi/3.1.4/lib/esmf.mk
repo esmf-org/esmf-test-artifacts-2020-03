688a0f6a35ed9898cd2aa80ecabb9da5cc4e5ae5
Role.Esmfmaint <esmfmaint@gmail.com>
2021-12-08 04:45:28 +0000

    update for test of gfortran_9.2.0_openmpi_g_develop with hash ESMF_8_3_0_beta_snapshot_04-7-gc7b07b1 on jet [ci skip]

--- develop/jet/gfortran/9.2.0/g/openmpi/3.1.4/lib/esmf.mk
+++ develop/jet/gfortran/9.2.0/g/openmpi/3.1.4/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-12-07 04:02:00
+build time -- 2021-12-08 04:02:44
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/jet/gfortran/9.2.0/g/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_g.bat
+++ develop/jet/gfortran/9.2.0/g/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_g.bat
@@ -1,4 +1,4 @@
-Wed Dec 8 04:02:52 GMT 2021
+Wed Dec 8 04:41:32 GMT 2021
 #!/bin/sh -l
 #SBATCH --account=hfv3gfs
 #SBATCH -o test-gfortran_9.2.0_openmpi_g.bat_%j.o
