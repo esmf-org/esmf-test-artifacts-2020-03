3d89aab3b5680abfed21eab2f56069d751743684
Role.Esmfmaint <esmfmaint@gmail.com>
2021-12-11 05:45:18 +0000

    update for test of intel_2019.5_mpi_g_develop with hash ESMF_8_3_0_beta_snapshot_04-8-g60a38ef on gaea [ci skip]

--- develop/gaea/intel/2019.5/g/mpi/7.7.11/lib/esmf.mk
+++ develop/gaea/intel/2019.5/g/mpi/7.7.11/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-12-10 00:28:30
+build time -- 2021-12-10 23:40:03
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/gaea/intel/2019.5/g/mpi/7.7.11/out/test-intel_2019.5_mpi_g.bat
+++ develop/gaea/intel/2019.5/g/mpi/7.7.11/out/test-intel_2019.5_mpi_g.bat
@@ -1,4 +1,4 @@
-Fri Dec 10 23:40:17 EST 2021
+Sat Dec 11 00:33:07 EST 2021
 #!/bin/sh -l
 #SBATCH --account=nggps_emc
 #SBATCH -o test-intel_2019.5_mpi_g.bat_%j.o
