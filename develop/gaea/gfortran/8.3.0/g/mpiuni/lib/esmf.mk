8c40b937f0f807e063490c8e977f641124ee200a
Role.Esmfmaint <esmfmaint@gmail.com>
2021-12-13 06:30:21 +0000

    update for test of gfortran_8.3.0_mpiuni_g_develop with hash ESMF_8_3_0_beta_snapshot_04-8-g60a38ef on gaea [ci skip]

--- develop/gaea/gfortran/8.3.0/g/mpiuni/lib/esmf.mk
+++ develop/gaea/gfortran/8.3.0/g/mpiuni/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-12-11 23:27:03
+build time -- 2021-12-12 23:40:04
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/gaea/gfortran/8.3.0/g/mpiuni/out/test-gfortran_8.3.0_mpiuni_g.bat
+++ develop/gaea/gfortran/8.3.0/g/mpiuni/out/test-gfortran_8.3.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Sun Dec 12 23:40:33 EST 2021
+Mon Dec 13 01:22:46 EST 2021
 #!/bin/sh -l
 #SBATCH --account=nggps_emc
 #SBATCH -o test-gfortran_8.3.0_mpiuni_g.bat_%j.o
