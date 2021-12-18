26c3010bbb559d9576a3df889ec48d51bacda19e
Role.Esmfmaint <esmfmaint@gmail.com>
2021-12-18 08:15:30 +0000

    update for test of pgi_18.1_intelmpi_g_develop with hash ESMF_8_3_0_beta_snapshot_05-1-g3e9d170 on hera [ci skip]

--- develop/hera/gfortran/9.2.0/g/openmpi/3.1.4/lib/esmf.mk
+++ develop/hera/gfortran/9.2.0/g/openmpi/3.1.4/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-12-17 06:02:58
+build time -- 2021-12-18 06:00:29
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/hera/gfortran/9.2.0b/g/intelmpi/2020/out/build-gfortran_9.2.0b_intelmpi_g.bat
+++ develop/hera/gfortran/9.2.0b/g/intelmpi/2020/out/build-gfortran_9.2.0b_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Sat Dec 18 06:02:18 UTC 2021
+Sat Dec 18 08:02:47 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o build-gfortran_9.2.0b_intelmpi_g.bat_%j.o
