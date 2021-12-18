a509d08a91d22a9f2a0b7f50564ede706b25eb08
Role.Esmfmaint <esmfmaint@gmail.com>
2021-12-18 02:00:24 +0000

    update for test of gfortran_10.3.0_mpich3_g_develop with hash ESMF_8_3_0_beta_snapshot_05-1-g3e9d170 on acorn [ci skip]

--- develop/acorn/gfortran/10.3.0/O/mpich3/8.1.7/lib/esmf.mk
+++ develop/acorn/gfortran/10.3.0/O/mpich3/8.1.7/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-12-17 01:28:15
+build time -- 2021-12-18 01:28:15
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/acorn/gfortran/10.3.0/g/mpich3/8.1.7/lib/esmf.mk
+++ develop/acorn/gfortran/10.3.0/g/mpich3/8.1.7/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-12-17 01:28:43
+build time -- 2021-12-18 01:28:40
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
