3a3521649cfa6d0be41bfe4fd0577db31e3b4e2d
Role.Esmfmaint <esmfmaint@gmail.com>
2021-12-20 07:15:51 +0000

    update for test of intel_2019.0.5_intelmpi_g_develop with hash ESMF_8_3_0_beta_snapshot_05-1-g3e9d170 on s4 [ci skip]

--- develop/s4/intel/2019.0.5/g/intelmpi/19.0.5/lib/esmf.mk
+++ develop/s4/intel/2019.0.5/g/intelmpi/19.0.5/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-12-19 06:20:21
+build time -- 2021-12-20 06:24:31
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/s4/intel/2019.0.5/g/intelmpi/19.0.5/out/test-intel_2019.0.5_intelmpi_g.bat
+++ develop/s4/intel/2019.0.5/g/intelmpi/19.0.5/out/test-intel_2019.0.5_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Mon Dec 20 06:25:02 UTC 2021
+Mon Dec 20 07:09:19 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=star
 #SBATCH -o test-intel_2019.0.5_intelmpi_g.bat_%j.o
