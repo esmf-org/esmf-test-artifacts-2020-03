3682b95ebeeead0f79c365bf2d9a5569d6f42b79
Role.Esmfmaint <esmfmaint@gmail.com>
2021-12-21 06:45:49 +0000

    update for test of intel_18.0.4_mpiuni_g_develop with hash ESMF_8_3_0_beta_snapshot_05-1-g3e9d170 on hera [ci skip]

--- develop/hera/intel/18.0.4/O/mpiuni/lib/esmf.mk
+++ develop/hera/intel/18.0.4/O/mpiuni/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-12-20 06:00:33
+build time -- 2021-12-21 06:01:08
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/hera/intel/18.0.4/O/mpiuni/out/build-intel_18.0.4_mpiuni_O.bat
+++ develop/hera/intel/18.0.4/O/mpiuni/out/build-intel_18.0.4_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Tue Dec 21 06:01:29 UTC 2021
+Tue Dec 21 06:32:08 UTC 2021
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o build-intel_18.0.4_mpiuni_O.bat_%j.o
