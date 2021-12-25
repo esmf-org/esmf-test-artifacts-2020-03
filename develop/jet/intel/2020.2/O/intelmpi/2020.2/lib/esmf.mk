d2b3912ec972cd0e518cc03f0baa84f9db381e7a
Role.Esmfmaint <esmfmaint@gmail.com>
2021-12-25 05:15:14 +0000

    update for test of intel_2020.2_intelmpi_O_develop with hash ESMF_8_3_0_beta_snapshot_05-1-g3e9d170 on jet [ci skip]

--- develop/jet/intel/2020.2/O/intelmpi/2020.2/lib/esmf.mk
+++ develop/jet/intel/2020.2/O/intelmpi/2020.2/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-12-23 04:05:59
+build time -- 2021-12-25 04:05:21
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/jet/intel/2020.2/g/intelmpi/2020.2/lib/esmf.mk
+++ develop/jet/intel/2020.2/g/intelmpi/2020.2/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-12-23 04:01:05
+build time -- 2021-12-25 04:00:25
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
