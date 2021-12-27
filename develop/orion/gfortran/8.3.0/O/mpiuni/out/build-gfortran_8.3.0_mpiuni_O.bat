ded1ca567a34f9da3a8f0010b2f84d201896907f
Role.Esmfmaint <esmfmaint@gmail.com>
2021-12-27 09:45:17 +0000

    update for test of gfortran_8.3.0_openmpi_O_develop with hash ESMF_8_3_0_beta_snapshot_05-1-g3e9d170 on orion [ci skip]

--- develop/orion/gfortran/8.3.0/O/mpiuni/out/build-gfortran_8.3.0_mpiuni_O.bat
+++ develop/orion/gfortran/8.3.0/O/mpiuni/out/build-gfortran_8.3.0_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Mon Dec 27 03:12:19 CST 2021
+Mon Dec 27 03:38:38 CST 2021
 #!/bin/sh -l
 #SBATCH --account=da-cpu
 #SBATCH -o build-gfortran_8.3.0_mpiuni_O.bat_%j.o
--- develop/orion/gfortran/8.3.0/O/openmpi/4.0.2/lib/esmf.mk
+++ develop/orion/gfortran/8.3.0/O/openmpi/4.0.2/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2021-12-26 02:41:03
+build time -- 2021-12-27 03:13:08
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
