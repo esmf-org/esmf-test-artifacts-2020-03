726f39482c3517ce2014f3be7f63aae397709128
Role.Esmfmaint <esmfmaint@gmail.com>
2022-01-10 04:15:25 +0000

    update for build of intel_2020.2_mvapich2_O_develop with hash ESMF_8_3_0_beta_snapshot_05-1-g3e9d170 on jet [ci skip]

--- develop/jet/gfortran/9.2.0/g/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_g.bat
+++ develop/jet/gfortran/9.2.0/g/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_g.bat
@@ -1,4 +1,4 @@
-Sun Jan 9 04:41:22 GMT 2022
+Mon Jan 10 04:01:27 GMT 2022
 #!/bin/sh -l
 #SBATCH --account=hfv3gfs
 #SBATCH -o test-gfortran_9.2.0_openmpi_g.bat_%j.o
--- develop/jet/intel/2020.2/O/intelmpi/2020.2/out/build-intel_2020.2_intelmpi_O.bat
+++ develop/jet/intel/2020.2/O/intelmpi/2020.2/out/build-intel_2020.2_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Sun Jan 9 04:53:48 GMT 2022
+Mon Jan 10 04:06:04 GMT 2022
 #!/bin/sh -l
 #SBATCH --account=hfv3gfs
 #SBATCH -o build-intel_2020.2_intelmpi_O.bat_%j.o
