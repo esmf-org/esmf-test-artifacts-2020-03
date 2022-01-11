3849abf8a5a016604b08b8ffee6a15422ab14cea
Rocky Dunlap <dunlap@ucar.edu>
2022-01-11 16:59:23 -0700

    update for build of gfortran_9.3.0_mpich3_O_jedwards_pio_update with hash ESMF_8_3_0_beta_snapshot_05-61-g27baa29 on catania [ci skip]

---
+++ jedwards_pio_update/catania/gfortran/9.3.0/O/mpich3/3.3.2-custom/out/build-gfortran_9.3.0_mpich3_O.bat
@@ -0,0 +1,20 @@
+Tue Jan 11 16:59:22 MST 2022
+#!/bin/bash -l
+export JOBID=12345
+
+module use /project/esmf/stack/modulefiles
+module load gnu/9.3.0/compiler gnu/9.3.0/mpich3/3.3.2-custom gnu/9.3.0/netcdf-c/4.7.4
+module load gnu/9.3.0/netcdf-fortran/4.5.3 
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/Volumes/esmf/rocky/esmf-testing/gfortran_9.3.0_mpich3_O_jedwards_pio_update
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpich3
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 4 2>&1| tee build_$JOBID.log
+
