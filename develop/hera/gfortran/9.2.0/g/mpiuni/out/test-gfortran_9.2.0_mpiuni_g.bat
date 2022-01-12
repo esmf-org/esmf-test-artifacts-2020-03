cb27750a5e96e08db1c4b0b1a83d9fbe4fbbba6d
Role.Esmfmaint <esmfmaint@gmail.com>
2022-01-12 07:45:56 +0000

    update for test of intel_18.0.4_mpiuni_O_develop with hash ESMF_8_3_0_beta_snapshot_05-2-geb61916 on hera [ci skip]

--- develop/hera/gfortran/9.2.0/g/mpiuni/out/test-gfortran_9.2.0_mpiuni_g.bat
+++ develop/hera/gfortran/9.2.0/g/mpiuni/out/test-gfortran_9.2.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Wed Jan 12 07:00:49 UTC 2022
+Wed Jan 12 07:32:46 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-gfortran_9.2.0_mpiuni_g.bat_%j.o
--- develop/hera/intel/18.0.4/O/mpiuni/out/build-intel_18.0.4_mpiuni_O.bat
+++ develop/hera/intel/18.0.4/O/mpiuni/out/build-intel_18.0.4_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Wed Jan 12 07:06:55 UTC 2022
+Wed Jan 12 07:37:59 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o build-intel_18.0.4_mpiuni_O.bat_%j.o
