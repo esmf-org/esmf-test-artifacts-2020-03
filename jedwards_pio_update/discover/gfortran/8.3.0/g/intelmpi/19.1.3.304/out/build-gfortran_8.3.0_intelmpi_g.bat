0e471d40c616c660874025511992550e7fcd3d60
Mark.Potts <Mark.Potts@noaa.gov>
2022-01-12 01:12:02 -0500

    update for build of gfortran_8.3.0_intelmpi_g_jedwards_pio_update with hash ESMF_8_3_0_beta_snapshot_05-64-gb869509 on discover [ci skip]

--- jedwards_pio_update/discover/gfortran/8.3.0/g/intelmpi/19.1.3.304/out/build-gfortran_8.3.0_intelmpi_g.bat
+++ jedwards_pio_update/discover/gfortran/8.3.0/g/intelmpi/19.1.3.304/out/build-gfortran_8.3.0_intelmpi_g.bat
@@ -1,8 +1,8 @@
-Wed Jan 12 01:11:45 EST 2022
+Wed Jan 12 01:11:54 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
-#SBATCH -o build-gfortran_10.1.0_intelmpi_O.bat_%j.o
-#SBATCH -e build-gfortran_10.1.0_intelmpi_O.bat_%j.e
+#SBATCH -o build-gfortran_8.3.0_intelmpi_g.bat_%j.o
+#SBATCH -e build-gfortran_8.3.0_intelmpi_g.bat_%j.e
 #SBATCH --time=1:00:00
 #SBATCH --partition=compute
 #SBATCH --qos=allnccs
@@ -10,17 +10,16 @@ Wed Jan 12 01:11:45 EST 2022
 #SBATCH --ntasks-per-node=28
 #SBATCH --exclusive
 export JOBID=$SLURM_JOBID
-module load comp/gcc/10.1.0 mpi/impi/19.1.3.304 
+module load comp/gcc/8.3.0 mpi/impi/19.1.3.304 
 
 module list >& module-build.log
 
 set -x
 
-export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
-export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/gfortran_10.1.0_intelmpi_O_develop
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/gfortran_8.3.0_intelmpi_g_jedwards_pio_update
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=intelmpi
-export ESMF_BOPT='O'
+export ESMF_BOPT='g'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
 make -j 28 2>&1| tee build_$JOBID.log
