2c982803501759d42e3bfd5e338d14238486915c
Role.Esmfmaint <esmfmaint@gmail.com>
2022-01-12 07:01:08 +0000

    update for test of intel_2020_intelmpi_g_develop with hash ESMF_8_3_0_beta_snapshot_05-2-geb61916 on discover [ci skip]

--- develop/discover/intel/2020/O/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_O.bat
+++ develop/discover/intel/2020/O/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Jan 12 01:14:10 EST 2022
+Wed Jan 12 01:52:48 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-intel_2020_intelmpi_O.bat_%j.o
--- develop/discover/intel/2020/g/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_g.bat
+++ develop/discover/intel/2020/g/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Wed Jan 12 01:14:47 EST 2022
+Wed Jan 12 01:57:26 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-intel_2020_intelmpi_g.bat_%j.o
