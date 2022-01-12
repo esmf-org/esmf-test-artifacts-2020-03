22cfc817f307622a3761565ef0073618e234983c
Mark.Potts <Mark.Potts@noaa.gov>
2022-01-12 01:13:46 -0500

    update for build of gfortran_8.3.0_mpiuni_g_develop with hash ESMF_8_3_0_beta_snapshot_05-2-geb61916 on discover [ci skip]

--- develop/discover/gfortran/8.3.0/g/mpiuni/out/build-gfortran_8.3.0_mpiuni_g.bat
+++ develop/discover/gfortran/8.3.0/g/mpiuni/out/build-gfortran_8.3.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Tue Jan 11 01:37:47 EST 2022
+Wed Jan 12 01:13:36 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-gfortran_8.3.0_mpiuni_g.bat_%j.o
--- jedwards_pio_update/discover/intel/2020/O/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_O.bat
+++ jedwards_pio_update/discover/intel/2020/O/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_O.bat
@@ -1,8 +1,8 @@
-Wed Jan 12 01:13:36 EST 2022
+Wed Jan 12 01:13:31 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
-#SBATCH -o test-gfortran_8.3.0_mpiuni_g.bat_%j.o
-#SBATCH -e test-gfortran_8.3.0_mpiuni_g.bat_%j.e
+#SBATCH -o test-intel_2020_intelmpi_O.bat_%j.o
+#SBATCH -e test-intel_2020_intelmpi_O.bat_%j.e
 #SBATCH --time=1:00:00
 #SBATCH --partition=compute
 #SBATCH --qos=allnccs
@@ -10,16 +10,18 @@ Wed Jan 12 01:13:36 EST 2022
 #SBATCH --ntasks-per-node=28
 #SBATCH --exclusive
 export JOBID=$SLURM_JOBID
-module load comp/gcc/8.3.0  
-
+module load comp/intel/19.1.3.304 mpi/impi/19.1.3.304 netcdf4/4.7.4
+module load hdf5/1.13.0 
 module list >& module-test.log
 
 set -x
+export ESMF_NETCDF=nc-config
 
-export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/gfortran_8.3.0_mpiuni_g_develop
-export ESMF_COMPILER=gfortran
-export ESMF_COMM=mpiuni
-export ESMF_BOPT='g'
+export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf -lhdf5_hl -lhdf5"
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/intel_2020_intelmpi_O_jedwards_pio_update
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='O'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
