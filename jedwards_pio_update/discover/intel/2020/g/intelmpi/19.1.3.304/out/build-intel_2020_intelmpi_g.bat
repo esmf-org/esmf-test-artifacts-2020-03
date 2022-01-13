775ed6a68816ab022e6eeb5160ffcac567955505
Mark.Potts <Mark.Potts@noaa.gov>
2022-01-13 01:59:42 -0500

    update for test of intel_2020_intelmpi_g_jedwards_pio_update with hash ESMF_8_3_0_beta_snapshot_05-69-g4b689dc on discover [ci skip]

--- jedwards_pio_update/discover/intel/2020/g/intelmpi/19.1.3.304/out/build-intel_2020_intelmpi_g.bat
+++ jedwards_pio_update/discover/intel/2020/g/intelmpi/19.1.3.304/out/build-intel_2020_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Thu Jan 13 01:15:31 EST 2022
+Thu Jan 13 01:59:08 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-intel_2020_intelmpi_g.bat_%j.o
--- jedwards_pio_update/discover/intel/2020/g/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_g.bat
+++ jedwards_pio_update/discover/intel/2020/g/intelmpi/19.1.3.304/out/test-intel_2020_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Thu Jan 13 01:15:32 EST 2022
+Thu Jan 13 01:59:08 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-intel_2020_intelmpi_g.bat_%j.o
