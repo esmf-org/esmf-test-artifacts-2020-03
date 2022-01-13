e3e3381b26bf9d82150ec895e0fd1fc41790e43c
Ryan Long <ryan.long@noaa.gov>
2022-01-12 21:15:47 -0700

    'updated summary for hash ESMF_8_3_0_beta_snapshot_05-3-g889e709 on developjet'

--- develop/jet/gfortran/9.2.0/g/openmpi/3.1.4/out/build-gfortran_9.2.0_openmpi_g.bat
+++ develop/jet/gfortran/9.2.0/g/openmpi/3.1.4/out/build-gfortran_9.2.0_openmpi_g.bat
@@ -1,4 +1,4 @@
-Wed Jan 12 04:38:26 GMT 2022
+Thu Jan 13 04:01:16 GMT 2022
 #!/bin/sh -l
 #SBATCH --account=hfv3gfs
 #SBATCH -o build-gfortran_9.2.0_openmpi_g.bat_%j.o
--- develop/jet/intel/2020.2/O/intelmpi/2020.2/out/test-intel_2020.2_intelmpi_O.bat
+++ develop/jet/intel/2020.2/O/intelmpi/2020.2/out/test-intel_2020.2_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Jan 12 04:52:09 GMT 2022
+Thu Jan 13 04:05:35 GMT 2022
 #!/bin/sh -l
 #SBATCH --account=hfv3gfs
 #SBATCH -o test-intel_2020.2_intelmpi_O.bat_%j.o
