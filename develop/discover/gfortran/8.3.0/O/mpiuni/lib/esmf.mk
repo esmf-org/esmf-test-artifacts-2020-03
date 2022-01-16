06fd9047f7832b794ed0751d1071d00a8e9ec4a3
Ryan Long <ryan.long@noaa.gov>
2022-01-15 23:31:05 -0700

    'updated summary for hash ESMF_8_3_0_beta_snapshot_05-28-g1022def on developdiscover'

--- develop/discover/gfortran/8.3.0/O/mpiuni/lib/esmf.mk
+++ develop/discover/gfortran/8.3.0/O/mpiuni/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2022-01-15 01:11:14.697441
+build time -- 2022-01-16 01:06:55.571531
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/discover/gfortran/8.3.0/O/mpiuni/out/test-gfortran_8.3.0_mpiuni_O.bat
+++ develop/discover/gfortran/8.3.0/O/mpiuni/out/test-gfortran_8.3.0_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Sun Jan 16 01:06:58 EST 2022
+Sun Jan 16 01:27:34 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-gfortran_8.3.0_mpiuni_O.bat_%j.o
