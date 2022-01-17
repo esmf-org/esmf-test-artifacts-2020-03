16fb8303990d1bd1407ca0ac8d91cc1edf25ed1e
Ryan Long <ryan.long@noaa.gov>
2022-01-17 01:10:33 -0700

    'updated summary for hash ESMF_8_3_0_beta_snapshot_05-28-g1022def on develophera'

--- develop/hera/gfortran/9.2.0/g/openmpi/3.1.4/lib/esmf.mk
+++ develop/hera/gfortran/9.2.0/g/openmpi/3.1.4/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2022-01-16 06:03:23
+build time -- 2022-01-17 06:03:37
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/hera/gfortran/9.2.0b/g/intelmpi/2020/lib/esmf.mk
+++ develop/hera/gfortran/9.2.0b/g/intelmpi/2020/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2022-01-16 06:04:53
+build time -- 2022-01-17 06:04:33
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
