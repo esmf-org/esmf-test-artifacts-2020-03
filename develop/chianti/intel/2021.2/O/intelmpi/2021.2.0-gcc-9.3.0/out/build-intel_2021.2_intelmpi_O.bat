aedb7c214583d3cf603de26a2a96ae01d27a06bf
Ryan Long <ryan.long@noaa.gov>
2022-01-20 23:20:45 -0700

    'updated summary for hash ESMF_8_3_0_beta_snapshot_06-7-gdaedd10 on developchianti'

--- develop/chianti/intel/2021.2/O/intelmpi/2021.2.0-gcc-9.3.0/out/build-intel_2021.2_intelmpi_O.bat
+++ develop/chianti/intel/2021.2/O/intelmpi/2021.2.0-gcc-9.3.0/out/build-intel_2021.2_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Fri Jan 21 01:14:35 EST 2022
+Fri Jan 21 01:15:55 EST 2022
 #!/bin/bash -l
 export JOBID=12345
 module load intel-oneapi-compilers/2021.2.0-gcc-9.3.0 intel-oneapi-mpi/2021.2.0-gcc-9.3.0 netcdf-c/4.8.0-intel-2021.2.0
