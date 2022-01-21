772316ad4d9dd79004cdbca64660130376fa62b3
Role.Esmfmaint <esmfmaint@gmail.com>
2022-01-21 08:13:25 +0000

    update for test of gfortran_9.2.0b_intelmpi_O_develop with hash ESMF_8_3_0_beta_snapshot_06-7-gdaedd10 on hera [ci skip]

--- develop/hera/gfortran/9.2.0/O/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_O.bat
+++ develop/hera/gfortran/9.2.0/O/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_O.bat
@@ -1,4 +1,4 @@
-Fri Jan 21 05:56:30 UTC 2022
+Fri Jan 21 08:13:10 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-gfortran_9.2.0_openmpi_O.bat_%j.o
--- develop/hera/gfortran/9.2.0b/O/intelmpi/2020/out/test-gfortran_9.2.0b_intelmpi_O.bat
+++ develop/hera/gfortran/9.2.0b/O/intelmpi/2020/out/test-gfortran_9.2.0b_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Fri Jan 21 05:58:33 UTC 2022
+Fri Jan 21 08:13:10 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-gfortran_9.2.0b_intelmpi_O.bat_%j.o
