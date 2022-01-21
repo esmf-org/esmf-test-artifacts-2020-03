bba8a53fed34e568215bd1af2af463c1d98af6e6
Mark.Potts <Mark.Potts@noaa.gov>
2022-01-21 01:48:16 -0500

    update for test of intel_2020_mpt_O_develop with hash ESMF_8_3_0_beta_snapshot_06-7-gdaedd10 on discover [ci skip]

--- develop/discover/gfortran/8.3.0/g/mpt/2.17/out/test-gfortran_8.3.0_mpt_g.bat
+++ develop/discover/gfortran/8.3.0/g/mpt/2.17/out/test-gfortran_8.3.0_mpt_g.bat
@@ -1,4 +1,4 @@
-Fri Jan 21 01:17:20 EST 2022
+Fri Jan 21 01:46:56 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-gfortran_8.3.0_mpt_g.bat_%j.o
--- jedwards_pio_update/discover/intel/2020/O/mpt/2.17/out/build-intel_2020_mpt_O.bat
+++ jedwards_pio_update/discover/intel/2020/O/mpt/2.17/out/build-intel_2020_mpt_O.bat
@@ -1,4 +1,4 @@
-Fri Jan 21 01:14:42 EST 2022
+Fri Jan 21 01:46:48 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-intel_2020_mpt_O.bat_%j.o
