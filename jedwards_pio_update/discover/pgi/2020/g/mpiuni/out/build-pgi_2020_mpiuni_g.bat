a51cf8bbda2a80dd8a96482d14b258652382671d
Mark.Potts <Mark.Potts@noaa.gov>
2022-01-22 03:04:33 -0500

    update for test of pgi_2020_mpiuni_g_jedwards_pio_update with hash ESMF_8_3_0_beta_snapshot_06-70-g65e948c on discover [ci skip]

--- jedwards_pio_update/discover/pgi/2020/g/mpiuni/out/build-pgi_2020_mpiuni_g.bat
+++ jedwards_pio_update/discover/pgi/2020/g/mpiuni/out/build-pgi_2020_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Sat Jan 22 01:45:50 EST 2022
+Sat Jan 22 03:04:09 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-pgi_2020_mpiuni_g.bat_%j.o
--- jedwards_pio_update/discover/pgi/2020/g/mpiuni/out/test-pgi_2020_mpiuni_g.bat
+++ jedwards_pio_update/discover/pgi/2020/g/mpiuni/out/test-pgi_2020_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Sat Jan 22 01:45:51 EST 2022
+Sat Jan 22 03:04:09 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-pgi_2020_mpiuni_g.bat_%j.o
