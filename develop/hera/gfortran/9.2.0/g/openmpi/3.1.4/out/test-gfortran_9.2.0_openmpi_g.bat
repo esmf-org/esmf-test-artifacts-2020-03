169a05b8014ff3c99f9df73cda58b0e8511ca62f
Ryan Long <ryan.long@noaa.gov>
2022-01-22 23:05:37 -0700

    'updated summary for hash ESMF_8_3_0_beta_snapshot_06-7-gdaedd10 on develophera'

--- develop/hera/gfortran/9.2.0/g/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_g.bat
+++ develop/hera/gfortran/9.2.0/g/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_g.bat
@@ -1,4 +1,4 @@
-Sat Jan 22 08:05:56 UTC 2022
+Sun Jan 23 06:01:21 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-gfortran_9.2.0_openmpi_g.bat_%j.o
--- develop/hera/intel/18.0.4/O/intelmpi/2018.4.274/out/build-intel_18.0.4_intelmpi_O.bat
+++ develop/hera/intel/18.0.4/O/intelmpi/2018.4.274/out/build-intel_18.0.4_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Sat Jan 22 07:01:39 UTC 2022
+Sun Jan 23 06:01:53 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o build-intel_18.0.4_intelmpi_O.bat_%j.o
