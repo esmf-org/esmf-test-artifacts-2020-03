7304229956070a1b182d281c3a36f99747a9bd9e
Mark.Potts <Mark.Potts@noaa.gov>
2022-01-24 01:08:49 -0500

    update for test of gfortran_8.3.0_mpiuni_O_develop with hash ESMF_8_3_0_beta_snapshot_06-7-gdaedd10 on discover [ci skip]

--- develop/discover/gfortran/8.3.0/O/mpiuni/out/test-gfortran_8.3.0_mpiuni_O.bat
+++ develop/discover/gfortran/8.3.0/O/mpiuni/out/test-gfortran_8.3.0_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Mon Jan 24 01:06:07 EST 2022
+Mon Jan 24 01:08:36 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-gfortran_8.3.0_mpiuni_O.bat_%j.o
--- jedwards_pio_update/discover/gfortran/8.3.0/O/intelmpi/19.1.3.304/out/test-gfortran_8.3.0_intelmpi_O.bat
+++ jedwards_pio_update/discover/gfortran/8.3.0/O/intelmpi/19.1.3.304/out/test-gfortran_8.3.0_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Sun Jan 23 01:37:58 EST 2022
+Mon Jan 24 01:08:21 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-gfortran_8.3.0_intelmpi_O.bat_%j.o
