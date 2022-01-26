56d059c79eee8212857b5a019b5d0a752281cd05
Mark Potts <mpotts@redlineperf.com>
2022-01-26 02:36:27 -0600

    update for build of gfortran_8.3.0_openmpi_O_develop with hash ESMF_8_3_0_beta_snapshot_06-9-gd3f8b21 on orion [ci skip]

--- develop/orion/intel/2020/g/intelmpi/2020.2/out/build-intel_2020_intelmpi_g.bat
+++ develop/orion/intel/2020/g/intelmpi/2020.2/out/build-intel_2020_intelmpi_g.bat
@@ -1,8 +1,8 @@
-Wed Jan 26 02:36:23 CST 2022
+Wed Jan 26 02:36:09 CST 2022
 #!/bin/sh -l
 #SBATCH --account=da-cpu
-#SBATCH -o build-gfortran_8.3.0_openmpi_O.bat_%j.o
-#SBATCH -e build-gfortran_8.3.0_openmpi_O.bat_%j.e
+#SBATCH -o build-intel_2020_intelmpi_g.bat_%j.o
+#SBATCH -e build-intel_2020_intelmpi_g.bat_%j.e
 #SBATCH --time=1:00:00
 #SBATCH --partition=orion
 #SBATCH --qos=batch
@@ -12,18 +12,17 @@ Wed Jan 26 02:36:23 CST 2022
 export JOBID=$SLURM_JOBID
 
 module load intelpython3 cmake
-module load gcc/8.3.0 openmpi/4.0.2 netcdf/4.7.4
+module load intel/2020.2 impi/2020.2 netcdf/4.7.4
 module list >& module-build.log
 
 set -x
 export ESMF_NETCDF=nc-config
 
-export LD_PRELOAD=/apps/gcc-8/gcc-8.3.0/lib64/libstdc++.so
 tar xvfz ~/pytest-input.tar.gz
-export ESMF_DIR=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_openmpi_O_develop
-export ESMF_COMPILER=gfortran
-export ESMF_COMM=openmpi
-export ESMF_BOPT='O'
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/intel_2020_intelmpi_g_develop
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='g'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
 make -j 40 2>&1| tee build_$JOBID.log
