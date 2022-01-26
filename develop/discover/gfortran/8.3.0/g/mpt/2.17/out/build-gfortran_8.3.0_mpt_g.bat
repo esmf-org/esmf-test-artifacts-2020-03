c1ccad611566c93b6b1c9d566bd00b88afe10a71
Ryan Long <ryan.long@noaa.gov>
2022-01-25 23:51:50 -0700

    'updated summary for hash ESMF_8_3_0_beta_snapshot_06-9-gd3f8b21 on developdiscover'

--- develop/discover/gfortran/8.3.0/g/mpt/2.17/out/build-gfortran_8.3.0_mpt_g.bat
+++ develop/discover/gfortran/8.3.0/g/mpt/2.17/out/build-gfortran_8.3.0_mpt_g.bat
@@ -1,4 +1,4 @@
-Wed Jan 26 01:19:51 EST 2022
+Wed Jan 26 01:46:57 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-gfortran_8.3.0_mpt_g.bat_%j.o
--- develop/discover/intel/2020/O/mpt/2.17/lib/esmf.mk
+++ develop/discover/intel/2020/O/mpt/2.17/lib/esmf.mk
@@ -1,4 +1,4 @@
-build time -- 2022-01-25 01:15:31.222895
+build time -- 2022-01-26 01:15:04.854724
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
