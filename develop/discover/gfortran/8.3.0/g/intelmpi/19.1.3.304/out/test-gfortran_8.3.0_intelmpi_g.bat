843e844a787cfa6d92560f8cca86f96c0d13a8be
Ryan Long <ryan.long@noaa.gov>
2022-01-26 23:16:17 -0700

    'updated summary for hash ESMF_8_3_0_beta_snapshot_06-10-gce27d44 on developdiscover'

--- develop/discover/gfortran/8.3.0/g/intelmpi/19.1.3.304/out/test-gfortran_8.3.0_intelmpi_g.bat
+++ develop/discover/gfortran/8.3.0/g/intelmpi/19.1.3.304/out/test-gfortran_8.3.0_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Wed Jan 26 01:53:10 EST 2022
+Thu Jan 27 01:13:49 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-gfortran_8.3.0_intelmpi_g.bat_%j.o
--- develop/discover/intel/2020/O/mpt/2.17/out/build-intel_2020_mpt_O.bat
+++ develop/discover/intel/2020/O/mpt/2.17/out/build-intel_2020_mpt_O.bat
@@ -1,4 +1,4 @@
-Wed Jan 26 01:45:24 EST 2022
+Thu Jan 27 01:14:26 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-intel_2020_mpt_O.bat_%j.o
