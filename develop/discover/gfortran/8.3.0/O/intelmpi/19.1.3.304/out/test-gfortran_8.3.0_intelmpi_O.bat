9c004eb2bcda90cec68accd8ac34c44916b052f7
Mark.Potts <Mark.Potts@noaa.gov>
2022-01-29 01:09:15 -0500

    update for build of gfortran_8.3.0_intelmpi_O_develop with hash ESMF_8_3_0_beta_snapshot_06-10-gce27d44 on discover [ci skip]

--- develop/discover/gfortran/8.3.0/O/intelmpi/19.1.3.304/out/test-gfortran_8.3.0_intelmpi_O.bat
+++ develop/discover/gfortran/8.3.0/O/intelmpi/19.1.3.304/out/test-gfortran_8.3.0_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Fri Jan 28 01:38:40 EST 2022
+Sat Jan 29 01:08:55 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-gfortran_8.3.0_intelmpi_O.bat_%j.o
--- jedwards_pio_update/discover/gfortran/8.3.0/O/mpt/2.17/out/test-gfortran_8.3.0_mpt_O.bat
+++ jedwards_pio_update/discover/gfortran/8.3.0/O/mpt/2.17/out/test-gfortran_8.3.0_mpt_O.bat
@@ -1,4 +1,4 @@
-Fri Jan 28 01:31:07 EST 2022
+Sat Jan 29 01:08:24 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-gfortran_8.3.0_mpt_O.bat_%j.o
