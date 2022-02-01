ec458c6f5edcf324eac81b28f69f23bfe0696900
Ryan Long <ryan.long@noaa.gov>
2022-02-01 07:51:52 -0700

    update for test of intel_18.0.5_openmpi_g_develop with hash ESMF_8_3_0_beta_snapshot_06-10-gce27d44 on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_g.bat
+++ develop/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_g.bat
@@ -1,4 +1,4 @@
-Tue Feb 1 06:27:28 MST 2022
+Tue Feb 1 07:51:21 MST 2022
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_openmpi_g.bat
 #PBS -l walltime=1:00:00
--- develop/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_g.bat
+++ develop/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_g.bat
@@ -1,4 +1,4 @@
-Tue Feb 1 06:27:28 MST 2022
+Tue Feb 1 07:51:21 MST 2022
 #!/bin/sh -l
 #PBS -N test-intel_18.0.5_openmpi_g.bat
 #PBS -l walltime=3:00:00
