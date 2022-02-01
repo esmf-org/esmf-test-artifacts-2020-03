0d3b38a3f364c7cbbef976fbb8c529553397122a
Role.Esmfmaint <esmfmaint@gmail.com>
2022-02-01 06:26:55 +0000

    update for test of gfortran_9.2.0_mpiuni_g_develop with hash ESMF_8_3_0_beta_snapshot_06-10-gce27d44 on hera [ci skip]

--- develop/hera/gfortran/9.2.0/g/mpiuni/out/build-gfortran_9.2.0_mpiuni_g.bat
+++ develop/hera/gfortran/9.2.0/g/mpiuni/out/build-gfortran_9.2.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Tue Feb 1 06:01:05 UTC 2022
+Tue Feb 1 06:26:40 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o build-gfortran_9.2.0_mpiuni_g.bat_%j.o
--- develop/hera/gfortran/9.2.0/g/mpiuni/out/test-gfortran_9.2.0_mpiuni_g.bat
+++ develop/hera/gfortran/9.2.0/g/mpiuni/out/test-gfortran_9.2.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Tue Feb 1 06:01:05 UTC 2022
+Tue Feb 1 06:26:39 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-gfortran_9.2.0_mpiuni_g.bat_%j.o
