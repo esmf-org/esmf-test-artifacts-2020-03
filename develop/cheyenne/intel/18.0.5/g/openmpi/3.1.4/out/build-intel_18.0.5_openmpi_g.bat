93afa73a494186c9010f7ca81e5383b4f1bf63d3
Ryan Long <ryan.long@noaa.gov>
2022-02-03 06:27:29 -0700

    update for build of intel_18.0.5_openmpi_g_develop with hash ESMF_8_3_0_beta_snapshot_06-10-gce27d44 on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_g.bat
+++ develop/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_g.bat
@@ -1,21 +1,21 @@
-Mon Oct 4 07:14:06 MDT 2021
+Thu Feb 3 06:27:28 MST 2022
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_openmpi_g.bat
 #PBS -l walltime=1:00:00
 #PBS -l walltime=3:00:00
 #PBS -q regular
-#PBS -A p48503002
+#PBS -A p93300606
 #PBS -l select=1:ncpus=36:mpiprocs=36
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /glade/scratch/mpotts/intel_18.0.5_openmpi_g_develop
+cd /glade/scratch/rlong/esmf-testing/intel_18.0.5_openmpi_g_develop
 module load intel/18.0.5 openmpi/3.1.4 netcdf/4.6.3
 module list >& module-build.log
 
 set -x
 export ESMF_NETCDF=nc-config
 
-export ESMF_DIR=/glade/scratch/mpotts/intel_18.0.5_openmpi_g_develop
+export ESMF_DIR=/glade/scratch/rlong/esmf-testing/intel_18.0.5_openmpi_g_develop
 export ESMF_COMPILER=intel
 export ESMF_COMM=openmpi
 export ESMF_BOPT='g'
