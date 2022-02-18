feaf2fefb4fed982def519af841c2b818636999f
Mark A Potts <Mark.Potts@noaa.gov>
2022-02-17 23:52:33 -0500

    update for build of gfortran_8.3.0_mpi_g_develop with hash ESMF_8_3_0_beta_snapshot_06-16-g2298b2e on gaea [ci skip]

--- develop/gaea/gfortran/8.3.0/g/mpi/7.7.11/out/test-gfortran_8.3.0_mpi_g.bat
+++ develop/gaea/gfortran/8.3.0/g/mpi/7.7.11/out/test-gfortran_8.3.0_mpi_g.bat
@@ -1,4 +1,4 @@
-Thu Feb 17 01:41:32 EST 2022
+Thu Feb 17 23:52:32 EST 2022
 #!/bin/sh -l
 #SBATCH --account=nggps_emc
 #SBATCH -o test-gfortran_8.3.0_mpi_g.bat_%j.o
@@ -13,7 +13,7 @@ export JOBID=$SLURM_JOBID
 
 module unload PrgEnv-intel
 
-module load PrgEnv-gnu
+module load PrgEnv-gnu git/2.26.0
 module load gcc/8.3.0 cray-mpich/7.7.11 cray-netcdf/4.6.3.2
 module list >& module-test.log
 
