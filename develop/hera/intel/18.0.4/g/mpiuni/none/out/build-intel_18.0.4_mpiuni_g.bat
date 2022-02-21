47ecc7e6584085b4e52a4265b56a0d5b1f370c5d
Role.Esmfmaint <esmfmaint@gmail.com>
2022-02-21 06:47:54 +0000

    update for test of intel_18.0.4_mpiuni_g_develop with hash ESMF_8_3_0_beta_snapshot_06-17-gf79410c on hera [ci skip]

--- develop/hera/intel/18.0.4/g/mpiuni/none/out/build-intel_18.0.4_mpiuni_g.bat
+++ develop/hera/intel/18.0.4/g/mpiuni/none/out/build-intel_18.0.4_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Mon Feb 21 06:04:05 UTC 2022
+Mon Feb 21 06:44:16 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o build-intel_18.0.4_mpiuni_g.bat_%j.o
--- develop/hera/intel/18.0.4/g/mpiuni/none/out/test-intel_18.0.4_mpiuni_g.bat
+++ develop/hera/intel/18.0.4/g/mpiuni/none/out/test-intel_18.0.4_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Mon Feb 21 06:04:04 UTC 2022
+Mon Feb 21 06:44:16 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-intel_18.0.4_mpiuni_g.bat_%j.o
