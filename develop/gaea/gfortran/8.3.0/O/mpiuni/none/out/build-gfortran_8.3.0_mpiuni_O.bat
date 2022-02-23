9f329a19badebeeabf30974083f0b63ec48702fe
Mark A Potts <Mark.Potts@noaa.gov>
2022-02-23 00:35:33 -0500

    update for test of gfortran_8.3.0_mpiuni_O_develop with hash v8.3.0b07-12-g8913088 on gaea [ci skip]

--- develop/gaea/gfortran/8.3.0/O/mpiuni/none/out/build-gfortran_8.3.0_mpiuni_O.bat
+++ develop/gaea/gfortran/8.3.0/O/mpiuni/none/out/build-gfortran_8.3.0_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Tue Feb 22 23:23:30 EST 2022
+Wed Feb 23 00:35:01 EST 2022
 #!/bin/sh -l
 #SBATCH --account=nggps_emc
 #SBATCH -o build-gfortran_8.3.0_mpiuni_O.bat_%j.o
--- develop/gaea/gfortran/8.3.0/O/mpiuni/none/out/test-gfortran_8.3.0_mpiuni_O.bat
+++ develop/gaea/gfortran/8.3.0/O/mpiuni/none/out/test-gfortran_8.3.0_mpiuni_O.bat
@@ -1,4 +1,4 @@
-Tue Feb 22 23:23:31 EST 2022
+Wed Feb 23 00:35:01 EST 2022
 #!/bin/sh -l
 #SBATCH --account=nggps_emc
 #SBATCH -o test-gfortran_8.3.0_mpiuni_O.bat_%j.o
