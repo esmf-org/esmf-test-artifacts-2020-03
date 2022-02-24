24aa64e2ce6c2493a77144d24ccd8df888e78121
Ryan Long <ryan.long@noaa.gov>
2022-02-24 07:52:10 -0700

    update for test of intel_18.0.5_intelmpi_g_develop with hash v8.3.0b08-1-gd4bb168 on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/g/intelmpi/2018.4.274/out/build-intel_18.0.5_intelmpi_g.bat
+++ develop/cheyenne/intel/18.0.5/g/intelmpi/2018.4.274/out/build-intel_18.0.5_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Thu Feb 24 06:30:48 MST 2022
+Thu Feb 24 07:51:42 MST 2022
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_intelmpi_g.bat
 #PBS -l walltime=1:00:00
--- develop/cheyenne/intel/18.0.5/g/intelmpi/2018.4.274/out/test-intel_18.0.5_intelmpi_g.bat
+++ develop/cheyenne/intel/18.0.5/g/intelmpi/2018.4.274/out/test-intel_18.0.5_intelmpi_g.bat
@@ -1,4 +1,4 @@
-Thu Feb 24 06:30:48 MST 2022
+Thu Feb 24 07:51:42 MST 2022
 #!/bin/sh -l
 #PBS -N test-intel_18.0.5_intelmpi_g.bat
 #PBS -l walltime=3:00:00
