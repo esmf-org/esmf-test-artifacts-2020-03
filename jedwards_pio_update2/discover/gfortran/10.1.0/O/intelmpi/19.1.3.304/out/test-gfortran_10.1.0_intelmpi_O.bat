b86a8d4c9ac555ed1db604a804608a6908eeba27
Mark.Potts <Mark.Potts@noaa.gov>
2022-02-26 01:12:15 -0500

    update for build of gfortran_10.1.0_intelmpi_O_develop with hash v8.3.0b08-5-g64eb133 on discover [ci skip]

--- jedwards_pio_update2/discover/gfortran/10.1.0/O/intelmpi/19.1.3.304/out/test-gfortran_10.1.0_intelmpi_O.bat
+++ jedwards_pio_update2/discover/gfortran/10.1.0/O/intelmpi/19.1.3.304/out/test-gfortran_10.1.0_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Sat Feb 26 01:12:00 EST 2022
+Sat Feb 26 01:11:51 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-gfortran_10.1.0_intelmpi_O.bat_%j.o
@@ -17,7 +17,7 @@ module list >& module-test.log
 set -x
 
 export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
-export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/gfortran_10.1.0_intelmpi_O_develop
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/gfortran_10.1.0_intelmpi_O_jedwards_pio_update2
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=intelmpi
 export ESMF_BOPT='O'
