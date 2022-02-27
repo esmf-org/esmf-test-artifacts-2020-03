76d36eef5444caf9dc86d801b243f4dcf7aa70b0
Role.Esmfmaint <esmfmaint@gmail.com>
2022-02-27 06:00:36 +0000

    update for build of intel_18.0.4_intelmpi_O_develop with hash v8.3.0b08-5-g64eb133 on hera [ci skip]

--- develop/hera/intel/18.0.4/O/intelmpi/2018.4.274/out/build-intel_18.0.4_intelmpi_O.bat
+++ develop/hera/intel/18.0.4/O/intelmpi/2018.4.274/out/build-intel_18.0.4_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Sat Feb 26 07:01:18 UTC 2022
+Sun Feb 27 06:00:31 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o build-intel_18.0.4_intelmpi_O.bat_%j.o
--- develop/hera/intel/18.0.4/O/intelmpi/2018.4.274/out/test-intel_18.0.4_intelmpi_O.bat
+++ develop/hera/intel/18.0.4/O/intelmpi/2018.4.274/out/test-intel_18.0.4_intelmpi_O.bat
@@ -1,4 +1,4 @@
-Sat Feb 26 07:01:19 UTC 2022
+Sun Feb 27 06:00:32 UTC 2022
 #!/bin/sh -l
 #SBATCH --account=nems
 #SBATCH -o test-intel_18.0.4_intelmpi_O.bat_%j.o
