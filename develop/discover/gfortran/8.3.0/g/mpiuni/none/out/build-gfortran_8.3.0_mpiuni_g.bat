f3cea8c62883cd37964488332416fd3329ace39b
Mark.Potts <Mark.Potts@noaa.gov>
2022-02-27 01:21:31 -0500

    update for build of gfortran_8.3.0_mpiuni_g_jedwards_pio_update2 with hash v8.3.0b08-90-g763a128 on discover [ci skip]

--- develop/discover/gfortran/8.3.0/g/mpiuni/none/out/build-gfortran_8.3.0_mpiuni_g.bat
+++ develop/discover/gfortran/8.3.0/g/mpiuni/none/out/build-gfortran_8.3.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Sat Feb 26 01:43:10 EST 2022
+Sun Feb 27 01:21:16 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-gfortran_8.3.0_mpiuni_g.bat_%j.o
--- jedwards_pio_update2/discover/gfortran/8.3.0/g/mpiuni/none/out/build-gfortran_8.3.0_mpiuni_g.bat
+++ jedwards_pio_update2/discover/gfortran/8.3.0/g/mpiuni/none/out/build-gfortran_8.3.0_mpiuni_g.bat
@@ -1,4 +1,4 @@
-Sat Feb 26 01:42:59 EST 2022
+Sun Feb 27 01:21:22 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-gfortran_8.3.0_mpiuni_g.bat_%j.o
