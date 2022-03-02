a4283606f86ac419c7813efc91be75764b0e579d
Ryan Long <ryan.long@noaa.gov>
2022-03-02 07:44:53 -0700

    update for test of intel_18.0.5_openmpi_O_develop with hash v8.3.0b08-5-g64eb133 on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_O.bat
+++ develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Mar 2 06:27:00 MST 2022
+Wed Mar 2 07:44:25 MST 2022
 #!/bin/sh -l
 #PBS -N build-intel_18.0.5_openmpi_O.bat
 #PBS -l walltime=1:00:00
--- develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_O.bat
+++ develop/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_O.bat
@@ -1,4 +1,4 @@
-Wed Mar 2 06:27:00 MST 2022
+Wed Mar 2 07:44:25 MST 2022
 #!/bin/sh -l
 #PBS -N test-intel_18.0.5_openmpi_O.bat
 #PBS -l walltime=3:00:00
