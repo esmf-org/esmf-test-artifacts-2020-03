715b756822e2d79ce9502c50b6d4cccb56a3793c
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-02 01:21:01 -0500

    update for build of intel_2020_mpt_g_develop with hash v8.3.0b08-5-g64eb133 on discover [ci skip]

--- develop/discover/intel/2020/g/mpt/2.17/out/build-intel_2020_mpt_g.bat
+++ develop/discover/intel/2020/g/mpt/2.17/out/build-intel_2020_mpt_g.bat
@@ -1,4 +1,4 @@
-Tue Mar 1 01:51:53 EST 2022
+Wed Mar 2 01:20:46 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o build-intel_2020_mpt_g.bat_%j.o
--- develop/discover/intel/2020/g/mpt/2.17/out/test-intel_2020_mpt_g.bat
+++ develop/discover/intel/2020/g/mpt/2.17/out/test-intel_2020_mpt_g.bat
@@ -1,4 +1,4 @@
-Tue Mar 1 01:51:53 EST 2022
+Wed Mar 2 01:20:46 EST 2022
 #!/bin/sh -l
 #SBATCH --account=s2326
 #SBATCH -o test-intel_2020_mpt_g.bat_%j.o
