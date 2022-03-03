9956b80ec34f771b711fb006cb629ef5754c3d67
Mark.Potts <Mark.Potts@noaa.gov>
2022-03-03 08:31:06 -0500

    update for build of gfortran_9.2.0_intelmpi_g_develop with hash v8.3.0b08-5-g64eb133 on discover [ci skip]

---
+++ develop/discover/gfortran/9.2.0/g/intelmpi/2021.3.0/out/build-gfortran_9.2.0_intelmpi_g.bat
@@ -0,0 +1,26 @@
+Thu Mar 3 08:30:50 EST 2022
+#!/bin/sh -l
+#SBATCH --account=s2326
+#SBATCH -o build-gfortran_9.2.0_intelmpi_g.bat_%j.o
+#SBATCH -e build-gfortran_9.2.0_intelmpi_g.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+module load comp/gcc/9.2.0 mpi/impi/2021.3.0 netcdf4/4.7.2
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/gfortran_9.2.0_intelmpi_g_develop
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 28 2>&1| tee build_$JOBID.log
+
